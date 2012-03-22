d1d7620ad10f65a134f47f78c219ae19d314b6b9
Alex Jacoby <jacobyam@rose-hulman.edu>
2012-03-22 14:57:55 -0400

    Testing JUnit works. And testOne passes

---
+++ PrimeFactorsTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+public class PrimeFactorsTest {
+
+	@Test
+	public void testJUnit(){
+		assertTrue("Testing JUnit",true);
+	}
+}
---
+++ PrimeNumbers.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class PrimeNumbers {
+
+	public static ArrayList<Integer> generatePrimes(int i) {
+		// TODO Auto-generated method stub
+		return new ArrayList<Integer>();
+	}
+
+}
