07cd7de9ad5bf60ef0dea96dad89ba20fea36a17
Alex Jacoby <jacobyam@rose-hulman.edu>
2012-03-22 15:10:22 -0400

    TestFour works

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -7,6 +7,11 @@ public class PrimeNumbers {
 		// TODO Auto-generated method stub
 		ArrayList<Integer> ret = new ArrayList<Integer>();
 		
+		if(i%2 == 0){
+			ret.add(2);
+			i = i/2;
+		}
+		
 		if(i > 1){
 			ret.add(i);
 		}
